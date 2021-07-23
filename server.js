1c7884424d24059bf7c92e3ee2a6cb45a106d41b
hkidd <harrisonakidd@gmail.com>
2021-07-23 15:09:39 -0500

    Started server.js file

---
+++ server.js
@@ -0,0 +1,38 @@
+// Import and require express, mysql2, and inquirer (maybe for a different file?)
+const express = require('express');
+const mysql = require('mysql2');
+const inquirer = require('inquirer');
+
+const PORT = process.env.PORT || 3001;
+const app = express();
+
+// Express middleware
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+
+// Connect to database
+const db = mysql.createConnection(
+  {
+    host: 'localhost',
+    // MySQL username,
+    user: 'root',
+    // MySQL password
+    password: 'password',
+    database: ''
+  },
+  console.log(`Connected to the _____ database.`)
+);
+
+// Query database
+db.query('SELECT * FROM ', function (err, results) {
+  console.log(results);
+});
+
+// Default response for any other request (Not Found)
+app.use((req, res) => {
+  res.status(404).end();
+});
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
