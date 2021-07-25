cf5a56844ba72805e6a5be206a07403e5d240d8c
hkidd <harrisonakidd@gmail.com>
2021-07-25 16:39:37 -0500

    SQL updates

--- db/seeds.sql
+++ db/seeds.sql
@@ -5,7 +5,8 @@ VALUES (1, "Sales"),
        (3, "Finance"),
        (4, "Legal")
 
-INSERT INTO roles ()
+INSERT INTO roles (title, salary, department_id)
 VALUES 
 
-
+INSERT INTO employees (first_name, last_name, role_id, manager_id)
+VALUES 
